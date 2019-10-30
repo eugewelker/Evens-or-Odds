0ffa2923207a85e5e20d9c646db598af84d01acf
eugewelker <eugewelker88@gmail.com>
2019-10-30 14:04:16 +0000

    evens or odds app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import App from './components/App';
+import rootReducer from './reducers';
 import './index.css';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const store= createStore(rootReducer);
+
+console.log('store', store);
+console.log('store.getState()', store.getState());
+
+store.subscribe(()=> console.log('store.getState()',store.getState()));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+   document.getElementById('root')
+ );
